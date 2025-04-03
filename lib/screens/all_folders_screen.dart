76fd34020d07ff3ab71d29e0e9f5cd18938b0821
Musa Waseem <musaw650@gmail.com>
2025-04-03 00:42:00 -0400

    fixing see more collections

--- lib/screens/all_folders_screen.dart
+++ lib/screens/all_folders_screen.dart
@@ -1,8 +1,11 @@
 import 'package:first_project/model/playlist.dart';
+import 'package:first_project/screens/now_playing_content.dart';
 import 'package:first_project/widgets/playlist_manager_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:first_project/size_config.dart';
+import 'package:first_project/neu_box.dart';
 
-class AllFoldersScreen extends StatelessWidget {
+class AllFoldersScreen extends StatefulWidget {
   final List<QawlPlaylist> playlists;
   final bool isPersonal;
 
@@ -13,44 +16,74 @@ class AllFoldersScreen extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<AllFoldersScreen> createState() => _AllFoldersScreenState(playlists, isPersonal);
+}
+
+class _AllFoldersScreenState extends State<AllFoldersScreen> {
+  late List<QawlPlaylist> playlists;
+  final bool isPersonal;
+
+  _AllFoldersScreenState(List<QawlPlaylist> playlists, this.isPersonal) {
+    this.playlists = playlists;
+  }
+
+  @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("All Collections"),
-        backgroundColor: Colors.black,
-      ),
-      backgroundColor: Colors.black,
-      body: playlists.isEmpty
-          ? const Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.folder_outlined, size: 64, color: Colors.green),
-                  SizedBox(height: 16),
-                  Text(
-                    "No Collections found",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(children: [
+            // back button and menu button
+            const SizedBox(height: 50),
+            QawlBackButton(),
+
+            Container(
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "All Collections",
+                  style: TextStyle(
+                      fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            
+            if (playlists.isEmpty)
+              const Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.folder_outlined, size: 64, color: Colors.green),
+                    SizedBox(height: 16),
+                    Text(
+                      "No Collections found",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            else
+              for (var playlist in playlists)
+                Material(
+                  child: Padding(
+                    padding: const EdgeInsets.all(1.0),
+                    child: FolderListItem(
+                      playlist: playlist,
+                      isPersonal: isPersonal,
+                      onAddTracks: (_) {}, // Empty callback
+                      onRefresh: () {}, // Empty callback since we don't refresh this list
                     ),
                   ),
-                ],
-              ),
-            )
-          : ListView.builder(
-              padding: const EdgeInsets.all(8),
-              itemCount: playlists.length,
-              itemBuilder: (context, index) {
-                final playlist = playlists[index];
-                return FolderListItem(
-                  playlist: playlist,
-                  isPersonal: isPersonal,
-                  onAddTracks: (_) {}, // Empty callback
-                  onRefresh: () {}, // Empty callback since we don't refresh this list
-                );
-              },
-            ),
+                ),
+          ]),
+        ),
+      ),
     );
   }
 }
